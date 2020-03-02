0f6e4ed5ea5e0feb14f07e0407d541d6918fba9c
Esther <esther.hyunmi.kim@gmail.com>
2020-03-01 19:38:46 -0800

    Challeng

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -88,3 +88,170 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp	
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date	 
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), 
+			de.dept_no
+INTO retiring_count			
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.gender,
+		s.salary,
+		de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+ 
+ --List of Sales riterees
+ SELECT d.dept_no,
+ 		d.dept_name,
+ 		de.emp_no,
+		dep.first_name,
+		dep.last_name,
+		de.to_date
+INTO retired_sales
+FROM dept_emp as de
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+INNER JOIN dept_info as dep
+ON (de.emp_no = dep.emp_no)
+WHERE (d.dept_no = 'd007')
+ORDER BY de.to_date;
+
+SELECT * FROM retired_sales;
+
+ --List of Sales and Development
+ SELECT d.dept_no,
+ 		d.dept_name,
+ 		de.emp_no,
+		dep.first_name,
+		dep.last_name,
+		de.to_date
+INTO retired_sales
+FROM dept_emp as de
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no)
+INNER JOIN dept_info as dep
+ON (de.emp_no = dep.emp_no)
+WHERE d.dept_no IN ('d007', 'd005')
+ORDER BY de.to_date;
+ 		
+-- Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name	
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--Number of [titles] Retiring
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	s.salary
+INTO retiring_name
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no);
+
+SELECT * FROM retiring_name;
+
+--Employees most recent title
+DELETE FROM retiring_name 
+USING (SELECT emp_no, MAX(from_date) maxDate
+	   FROM retiring_name
+	   GROUP BY emp_no)
+AS keep
+WHERE keep.maxDate <> retiring_name.from_date
+AND keep.emp_no = retiring_name.emp_no;
+
+--Employees most recent title
+SELECT emp_no, first_name, last_name,  MAX(from_date) maxDate
+	   FROM retiring_name
+	   GROUP BY emp_no, first_name, last_name;
+
+--Retiree Mentors
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date
+Into mentor_list
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (ti.to_date > '2020-02-29');
+
+
